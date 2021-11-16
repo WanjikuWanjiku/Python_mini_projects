012d3173ec24c2f9b8bb9538f05595dc2245f89b
Wanjiku_Wanjiku <33546723+WanjikuWanjiku@users.noreply.github.com>
2021-11-16 09:01:26 +0300

    Update madlibsv1.py

--- madlibsv1.py
+++ madlibsv1.py
@@ -1,5 +1,7 @@
-# Write your code here :-)
 #madlibs v1
+
+#madlibs collects user input and inserts it into a text template to create a story. 
+
 #user input to be inserted in the madlibs.
 food_item_1 = input("Enter name of food: ")
 country_1 = input("Enter name of a country: ")
@@ -13,11 +15,13 @@ verb_2 = input("Enter a verb: ")
 colour_1 = input("Enter a colour: ")
 vegetable_1 = input("Enter a vegetable: ")
 
-#Using an f-string for the madlibs.
+#Using an f-string to embed user input into the madlibs template. 
+
 madlibs = f"The largest {food_item_1} in the world was discovered recently in {country_1}. \
 It is {comparative_1} than {country_2} and is as wide as {number_1} {vehicle_1}s. \
 The amazing thing is that it can {verb_1} more than {number_2} people. \
 If you get a chance, you can go and {verb_2} it at the {colour_1} {vegetable_1} museum."
 
 #print the madlibs
+
 print(madlibs)
